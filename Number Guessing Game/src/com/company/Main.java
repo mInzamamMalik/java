c5c491433fc6749baf59779995784671966d2b66
ahmed raza <a.raza27.ar@gmail.com>
2016-07-17 17:49:46 +0500

    number guessing game completed

--- Number Guessing Game/src/com/company/Main.java
+++ Number Guessing Game/src/com/company/Main.java
@@ -1,8 +1,77 @@
 package com.company;
 
+import java.util.Scanner;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    
+        Scanner input = new Scanner(System.in);
+
+
+        int credit = 0;
+        String choice;
+
+        print("\n\ndo you want to play game number guessing game?(y/n)");
+        choice = input.next();
+
+        do {
+            switch (choice) {
+
+                case "Y":
+                case "y":
+
+                    if (credit <= 0) {
+                        print("\nYour Trail has been expired, please enter credit(Max 10)");
+                        int tempInput = input.nextInt();
+                        if (tempInput > 10) {
+                            print("\nyou are exceeding Max credit, maximum 10 credits was allowed");
+                            break;
+                        } else {
+                            credit = tempInput;
+                        }
+                    }
+
+                    int randomNumber = ThreadLocalRandom.current().nextInt(1, 11);
+
+                    print("\nWe have generated a Random number, Please Select A Number if it match you win: ");
+                    int tempInput = input.nextInt();
+                    if (randomNumber == tempInput) {
+                        print("\nCongo!! You have Win the game, your have "+ credit+" Credits remaining");
+                    } else {
+                        credit--;
+                        print("\nSorry our the number was: " + randomNumber + "\nBetter luck next time, your have "+ credit+" Credits remaining");
+                    }
+
+                    break;
+
+                case "N":
+                case "n":
+                    print("\nThank You for playing, game is Terminating...");
+                    choice = "n";
+                    break;
+
+                default:
+                    print("\ninvalid input, please type 'y' or 'n' Erectly");
+            }
+
+            print("Your have "+ credit+" Credits remaining");
+            print("\nDo you want to play this interesting game again?(y/n)");
+            choice = input.next();
+
+        } while (choice.equalsIgnoreCase("Y"));
+
     }
+
+    public static void print(String line) {
+        System.out.println(line);
+    }
+    
 }
+
+
+
